87d3aea3137e2f843ccb55582f3eb8d6edf9a65f
= <briggs.dalin09@gmail.com>
2025-07-05 12:01:17 -0600

    Complete Wk01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,20 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // There are a double and int as parameters
+        // Create an array for multiples
+
+        double[] mult = new double[length];
+
+        // With a for bucle interate the length for the multiples
+        for (int i = 0; i < length; i++)
+        {
+            // Set the operation of multiples by the length
+            mult[i] = number * (i + 1);
+         }
+
+        // Return result
+        return mult; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +42,22 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // There are a List<int> and int
+        // First calculate the rotate amount is the correct amount
+        int rotateAmount = amount % data.Count;
+
+
+        // Create a list for the rotate
+        List<int> rotate = new List<int>(data.Count);
+
+        // Add the element after the rotation into the List rotate and then the new one, using AddRange and GetRange
+        rotate.AddRange(data.GetRange(data.Count - rotateAmount, rotateAmount));
+        rotate.AddRange(data.GetRange(0, data.Count - rotateAmount));
+
+        // In the list of data change it with the rotate List
+        data.Clear();
+        data.AddRange(rotate);
+
     }
 }
